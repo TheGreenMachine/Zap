d8d4a3e0d717a4709545587909e0e927e2aae7ba
Maxalexander-pi <34904974+Maxalexander-pi@users.noreply.github.com>
2022-01-24 19:58:04 -0600

    Use TCP server API

--- ZodiacVision/main.py
+++ ZodiacVision/main.py
@@ -1,5 +1,6 @@
 import cv2
 import yaml
+import socketserver
 
 from vision import networktables, camera, detect, stream, fps, socketserver
 import time
@@ -11,40 +12,12 @@ net = networktables.NetworkTables(data, path)
 net.setupCalib()
 
 visionCamera = camera.Camera()
-socket = socketserver.SocketServer(data, path, 1180)
 
 detector = detect.Detector(net, visionCamera)
 #streamer = stream.Streamer(data['stream']['port'])
 width = int(net.yml_data['stream']['line'])
 fpsCounter = fps.FPS()
 
-socket.startServer()
-
-
-
-
-
-while True:
-    fpsCounter.reset()
-    fpsCounter.start()
-    if net.update_exposure:
-        visionCamera.updateExposure(net)
-        net.update_exposure = False
-    # A new image is available if grab() returns SUCCESS
-    frame = visionCamera.read()
-    if net.vision_use:
-        cv2.imwrite('/home/jetson/ZodiacVision/capture/' + time.strftime("%Y%m%d-%H%M%S") + '.png', frame)
-        net.vision_use = False
-    mask = detector.preProcessFrame(frame)
-    if mask.all() == -1:
-        continue
-    contour, center_x, center_y = detector.findTarget(mask)
-
-    socket.listenMessage(1816 ,1, 1)
-
-    stream_image = detector.postProcess(frame, contour)
-    fpsCounter.update()
-    fpsCounter.stop()
     # stream_image = fps.putIterationsPerSec(stream_image, fpsCounter.fps())
     #
     # if net.line:
@@ -55,3 +28,50 @@ while True:
     #     streamer.write(mask)
     # else:
     #     streamer.write(stream_image)
+
+class VisionDispatcher(socketserver.BaseRequestHandler):
+    def handle(self):
+        mask = self.getMask()
+        center_x, center_y, distance = -1, -1, -1
+        if mask.all() != -1: # if -1 - give them that, let the client handle
+            contour, center_x, center_y = detector.findTarget(mask)
+            distance = camera.findTargetDistance(center_x, center_y)
+
+        msg = self.request.recv(1024).strip()
+        self.dispatchResponse(msg, center_x, center_y, distance)
+        fpsCounter.update()
+        fpsCounter.stop()
+
+    def reply(self, msg):
+        self.request.sendall(msg.encode())
+
+    def dispatchResponse(self, msg, center_x, center_y, distance):
+        if msg == 'distance':
+            self.reply(("distance|"+distance+"\n"))
+        elif msg == 'center_x':
+            self.reply("center_x|"+center_x+"\n")
+        elif msg == 'cxy':
+            self.reply(""+center_x+","+center_y)
+        elif msg == 'cxd':
+            self.reply(""+center_x+","+center_y+","+distance)
+        else:
+            self.reply('error|invalid request\n')
+        
+    def getMask():
+        fpsCounter.reset()
+        fpsCounter.start()
+        if net.update_exposure:
+            visionCamera.updateExposure(net)
+            net.update_exposure = False
+        # A new image is available if grab() returns SUCCESS
+        frame = visionCamera.read()
+        if net.vision_use:
+            cv2.imwrite('/home/jetson/ZodiacVision/capture/' + time.strftime("%Y%m%d-%H%M%S") + '.png', frame)
+            net.vision_use = False
+        return detector.preProcessFrame(frame)
+
+
+port = net.yml_data['main']['port']
+
+with socketserver.TCPServer(('localhost', port), VisionDispatcher) as server:
+    server.serve_forever()
