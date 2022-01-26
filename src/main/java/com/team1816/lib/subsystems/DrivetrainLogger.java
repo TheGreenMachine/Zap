48ba0a08a5ba4bb49521b2e847c68734989c4f62
David Day <dcday@themilwaukeeroad.com>
2022-01-25 20:50:05 -0600

    restore swerve logging

--- src/main/java/com/team1816/lib/subsystems/DrivetrainLogger.java
+++ src/main/java/com/team1816/lib/subsystems/DrivetrainLogger.java
@@ -1,51 +1,117 @@
 package com.team1816.lib.subsystems;
 
+import badlog.lib.BadLog;
+
 public class DrivetrainLogger {
 
     public static void init(TrackableDrivetrain drivetrain) {
+        var isSwerve = drivetrain instanceof SwerveDrivetrain;
         var subsystem = (Subsystem) drivetrain;
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/LeftActVel",
-            "NativeUnits",
-            drivetrain::getLeftVelocityNativeUnits,
-            "hide",
-            "join:Drivetrain/Velocities"
-        );
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/RightActVel",
-            "NativeUnits",
-            drivetrain::getRightVelocityNativeUnits,
-            "hide",
-            "join:Drivetrain/Velocities"
-        );
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/LeftVel",
-            "NativeUnits",
-            drivetrain::getLeftVelocityDemand,
-            "hide",
-            "join:Drivetrain/Velocities"
-        );
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/RightVel",
-            "NativeUnits",
-            drivetrain::getRightVelocityDemand,
-            "hide",
-            "join:Drivetrain/Velocities"
-        );
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/LeftError",
-            "NativeUnits",
-            drivetrain::getLeftVelocityError,
-            "hide",
-            "join:Drivetrain/VelocityError"
-        );
-        subsystem.CreateBadLogTopic(
-            "Drivetrain/RightError",
-            "NativeUnits",
-            drivetrain::getRightVelocityError,
-            "hide",
-            "join:Drivetrain/VelocityError"
-        );
+        if(isSwerve) {
+            for (ISwerveModule module : ((SwerveDrivetrain)drivetrain).getSwerveModules()) {
+                var name = module.getSubsystemName();
+                var prefix = "Drivetrain/" + name;
+
+                // Azimuth
+                BadLog.createTopic(
+                    prefix + "AzimuthPosition",
+                    "ticks",
+                    module::getAzimuthPosition,
+                    "hide",
+                    "join:Drivetrain/AzimuthPosition"
+                );
+                BadLog.createTopic(
+                    prefix + "AzimuthPositionDemand",
+                    "ticks",
+                    module::getAzimuthPositionDemand,
+                    "hide",
+                    "join:Drivetrain/AzimuthPosition"
+                );
+                BadLog.createTopic(
+                    prefix + "AzimuthError",
+                    "ticks",
+                    module::getAzimuthError,
+                    "hide",
+                    "join:Drivetrain/AzimuthError"
+                );
+
+                // Drive
+                BadLog.createTopic(
+                    prefix + "DriveVelocity",
+                    "ticks",
+                    module::getDriveVelocity,
+                    "hide",
+                    "join:Drivetrain/DriveVelocity"
+                );
+                BadLog.createTopic(
+                    prefix + "DriveVelocityDemand",
+                    "ticks",
+                    module::getDriveVelocityDemand,
+                    "hide",
+                    "join:Drivetrain/DriveVelocity"
+                );
+                BadLog.createTopic(
+                    prefix + "DriveDistance",
+                    "ticks",
+                    module::getDriveDistance,
+                    "hide",
+                    "join:Drivetrain/DriveDistance"
+                );
+                BadLog.createTopic(
+                    prefix + "DriveError",
+                    "ticks",
+                    module::getDriveError,
+                    "hide",
+                    "join:Drivetrain/DriveError"
+                );
+            }
+        }
+        else {
+
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/LeftActVel",
+                "NativeUnits",
+                drivetrain::getLeftVelocityNativeUnits,
+                "hide",
+                "join:Drivetrain/Velocities"
+            );
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/RightActVel",
+                "NativeUnits",
+                drivetrain::getRightVelocityNativeUnits,
+                "hide",
+                "join:Drivetrain/Velocities"
+            );
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/LeftVel",
+                "NativeUnits",
+                drivetrain::getLeftVelocityDemand,
+                "hide",
+                "join:Drivetrain/Velocities"
+            );
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/RightVel",
+                "NativeUnits",
+                drivetrain::getRightVelocityDemand,
+                "hide",
+                "join:Drivetrain/Velocities"
+            );
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/LeftError",
+                "NativeUnits",
+                drivetrain::getLeftVelocityError,
+                "hide",
+                "join:Drivetrain/VelocityError"
+            );
+            subsystem.CreateBadLogTopic(
+                "Drivetrain/RightError",
+                "NativeUnits",
+                drivetrain::getRightVelocityError,
+                "hide",
+                "join:Drivetrain/VelocityError"
+            );
+
+        }
         subsystem.CreateBadLogTopic(
             "Drivetrain/X Desired",
             "Inches",
