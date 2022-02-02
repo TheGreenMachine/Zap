1d28606e6be322160b8dde351203cd86459b9168
Ian McVann <ian.mcvann@gmail.com>
2022-02-01 21:08:14 -0600

    Add initial calibration server framework

--- ZodiacVision/scripts/socket_client_test.py
+++ ZodiacVision/scripts/socket_client_test.py
@@ -1,10 +1,10 @@
 import socket
 import sys
 
-address = ('10.18.16.16', 5802)
+address = ('localhost', 5802)
 data = ''.join(sys.argv[1:])
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(address)
-    s.send(b'point')
+    s.send(b'calib|EXPOSURE|10')
     received = s.recv(20000)
     print(received)
