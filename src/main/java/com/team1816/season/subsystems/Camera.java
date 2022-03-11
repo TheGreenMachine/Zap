b6762b736afff2e72d44e0822c574340fe9b5915
Maxalexander-pi <34904974+Maxalexander-pi@users.noreply.github.com>
2022-03-11 15:57:47 -0600

    Fix and clean up Camera

--- src/main/java/com/team1816/season/subsystems/Camera.java
+++ src/main/java/com/team1816/season/subsystems/Camera.java
@@ -3,17 +3,14 @@ package com.team1816.season.subsystems;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.team1816.lib.subsystems.Subsystem;
-import com.team1816.season.Constants;
-
-import java.io.*;
-import java.net.*;
+import com.team1816.lib.vision.VisionSocket;
 
 @Singleton
 public class Camera extends Subsystem {
 
     private static final String NAME = "camera";
 
-    private final String PROTOCOL_LINE = "\\|";
+    public final VisionSocket socket = new VisionSocket();
 
     // Components
     @Inject
@@ -24,145 +21,59 @@ public class Camera extends Subsystem {
     private static final double CAMERA_FOCAL_LENGTH = 350; // px
     private static final double VIDEO_WIDTH = 672.0; // px
     public static final double ALLOWABLE_AIM_ERROR = 0.2; // deg
-
-    // Sockets!
-    private Socket socket;
-    private BufferedReader socketIn;
-    private PrintWriter socketOut;
-    private Boolean cameraOn = false; // does this need to be a Boolean object?
-    private long needsReconnect = 0;
-
     public Camera() {
         super(NAME);
-    }
-
-    private String query(String message) throws IOException {
-        if (!cameraOn) return "";
-        if (socketOut == null || socketIn == null || socket == null || !socket.isConnected()) {
-            if (socket == null || !socket.isConnected()) {
-                System.out.println("Socket does not exist, reconnecting");
-                needsReconnect = System.currentTimeMillis();
-                return "";
-            }
-            System.out.println("Socket exists but streams do not, creating then");
-            socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socketOut = new PrintWriter(socket.getOutputStream(), true);
-        }
-        if (needsReconnect != 0) {
-            System.out.println("CONNECTION LOST: ATTEMPTING TO RECONNECT LINE 41 ");
-            return "";
-        }
-        if (cameraOn) {
-            socketOut.write(message + "\n");
-            socketOut.flush();
-            String out = socketIn.readLine();
-            if (out == null) return "";
-            return out;
-        }
-        return "";
-    }
-
-    private boolean socketConnect() {
-        if (!cameraOn) return true;
-        try {
-            socket = new Socket("10.18.16.16", 5802);
-            //socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), 5802), 10);
-            socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socketOut = new PrintWriter(socket.getOutputStream(), true);
-        } catch (IOException e) {
-            System.out.println("socket Connect error: " + e.getMessage());
-            needsReconnect = System.currentTimeMillis();
-            return false;
-        }
-        return true;
+        socket.setDebug(factory.getConstant(NAME, "debug") > 0);
     }
 
     public double getDeltaXAngle() {
-        if (!cameraOn) return 0;
-        try {
-            String line = query("center_x");
-            String[] coords = line.split(PROTOCOL_LINE);
-            if (coords.length < 2) {
-                return 0;
-            }
-            double x = Double.parseDouble(coords[1]);
-            if (x < 0) {
-                // Reset deltaX to 0 if contour not detected
-                return 0;
-            }
-            double deltaXPixels = (x - (VIDEO_WIDTH / 2)); // Calculate deltaX from center of screen
-            return Math.toDegrees(Math.atan2(deltaXPixels, CAMERA_FOCAL_LENGTH)) * 0.64;
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            needsReconnect = System.currentTimeMillis();
+        String[] coords = socket.request("center_x");
+        if (coords.length < 2) {
             return 0;
         }
+        double x = Double.parseDouble(coords[1]);
+        if (x < 0) {
+            // Reset deltaX to 0 if contour not detected
+            return 0;
+        }
+        double deltaXPixels = (x - (VIDEO_WIDTH / 2)); // Calculate deltaX from center of screen
+        return Math.toDegrees(Math.atan2(deltaXPixels, CAMERA_FOCAL_LENGTH)) * 0.64;
     }
 
     public double getDistance() {
-        if (!cameraOn) return 0;
-        try {
-            String line = query("distance");
-            System.out.println(line);
-            String[] parts = line.split(PROTOCOL_LINE);
-            if (parts.length < 2) {
-                System.out.println(parts);
-                return 0;
-            }
-            System.out.println("CAMERA LINE 91: getDistance() " + Double.parseDouble(parts[1]));
-            return Double.parseDouble(parts[1]);
-        } catch (IOException e) {
-            System.out.println("CAMERA EXCEPTION GET DISTANCE LINE 94 " + e.getMessage());
-            needsReconnect = System.currentTimeMillis();
+        String[] parts = socket.request("distance");
+        if (parts.length < 2) {
+            System.out.println(parts);
             return 0;
         }
+        System.out.println("CAMERA LINE 91: getDistance() " + Double.parseDouble(parts[1]));
+        return Double.parseDouble(parts[1]);
     }
 
     public double getRawCenterX() {
-        if (!cameraOn) return 0;
-        try {
-            String line = query("center_x");
-            String[] coords = line.split(PROTOCOL_LINE);
-            if (coords.length < 2) {
-                return 0;
-            }
-            return Double.parseDouble(coords[1]);
-        } catch (IOException e) {
-            needsReconnect = System.currentTimeMillis();
+        String[] coords = socket.request("center_x");
+        if (coords.length < 2) {
             return 0;
         }
+        return Double.parseDouble(coords[1]);
     }
 
     public void setEnabled(boolean enabled) {
         led.setCameraLed(enabled);
-        cameraOn = enabled;
-        if (!enabled) {
-            if (needsReconnect != 0) {
-                needsReconnect = 0;
-            }
-        } else {
-            socketConnect();
-        }
+        socket.setEnabled(enabled);
     }
 
     public boolean checkSystem() {
-        return needsReconnect != 0;
+        return true;
     }
 
     public void stop() {
-        try {
-            socket.close();
-        } catch (IOException e) {}
+        socket.close();
     }
 
     public void readFromHardware() {
-        if (!cameraOn) return;
-        // if more than 200ms, reconnect
-        if (needsReconnect != 0 && (System.currentTimeMillis() - needsReconnect) >= 2*Constants.kLooperDt) {
-            //            System.out.println("Reconnect attempt at " + System.currentTimeMillis());
-            if (socketConnect()) {
-                needsReconnect = 0;
-            }
+        if (socket.shouldReconnect()) {
+            socket.connect();
         }
     }
 }
