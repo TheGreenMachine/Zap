b9d78f59f965d3b9453735f223cef87d449badff
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-03-17 19:55:21 -0500

    temp

--- src/main/java/com/team1816/season/RobotState.java
+++ src/main/java/com/team1816/season/RobotState.java
@@ -17,6 +17,7 @@ public class RobotState {
     public ChassisSpeeds chassis_speeds = new ChassisSpeeds(0, 0, 0);
 
     // Superstructure ACTUAL states
+    public Point visionPoint = new Point();
     public Collector.COLLECTOR_STATE collectorState = Collector.COLLECTOR_STATE.STOP;
     public Shooter.SHOOTER_STATE shooterState = Shooter.SHOOTER_STATE.STOP;
     public Spindexer.SPIN_STATE spinState = Spindexer.SPIN_STATE.STOP;
@@ -85,5 +86,16 @@ public class RobotState {
                     Rotation2d.fromDegrees(getLatestFieldToTurret())
                 )
             );
+    }
+
+    private class Point{
+        public int cX;
+        public int cY;
+        public int dist;
+        public Point () {
+            cX = 0;
+            cY = 0;
+            dist = 0;
+        }
     }
 }
