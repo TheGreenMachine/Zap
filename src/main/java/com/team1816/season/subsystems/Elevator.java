86318e5b585a827c64b5e7df503412414ed77774
Nico Leibert <nicholasl23638@gmail.com>
2022-06-22 13:03:14 -0500

    made elevator a bit prettier

--- src/main/java/com/team1816/season/subsystems/Elevator.java
+++ src/main/java/com/team1816/season/subsystems/Elevator.java
@@ -15,27 +15,25 @@ public class Elevator extends Subsystem {
     private static final String NAME = "elevator";
 
     // Components
-    private final IMotorControllerEnhanced elevator;
+    private final IMotorControllerEnhanced elevatorMotor;
     private final DigitalInput ballSensor;
 
     // State
-    private double elevatorOutput;
-    private boolean outputsChanged;
+    private double desiredOutput;
     private double actualOutput;
+    private boolean outputsChanged;
     private STATE state = STATE.STOP;
 
-    // Constants
-    private final double MAX_TICKS;
     private final double ALLOWABLE_ERROR;
     private final double FLUSH;
     private final double INTAKE;
-    private double FIRE; // bear in mind this is overridden
+    private final double FIRE;
     private final boolean isVelocity;
     private final String pidSlot = "slot0";
 
     public Elevator() {
         super(NAME);
-        this.elevator = factory.getMotor(NAME, "elevator");
+        this.elevatorMotor = factory.getMotor(NAME, "elevator");
 
         PIDSlotConfiguration config = factory.getPidSlotConfig(NAME, pidSlot);
         this.ballSensor =
@@ -43,7 +41,8 @@ public class Elevator extends Subsystem {
 
         isVelocity = factory.getConstant(NAME, "isVelocity", 0) > 0;
 
-        MAX_TICKS = factory.getConstant(NAME, "maxTicks", 0);
+        // Constants
+        double MAX_TICKS = factory.getConstant(NAME, "maxTicks", 0);
         if (!isVelocity) {
             FLUSH = factory.getConstant(NAME, "flushPow", -0.5);
             FIRE = factory.getConstant(NAME, "firePow", 0.5);
@@ -56,20 +55,14 @@ public class Elevator extends Subsystem {
         ALLOWABLE_ERROR = config.allowableError;
     }
 
-    public void overridePower(double newFirePow) {
-        FIRE = newFirePow;
-    }
-
     private void setElevator(double elevatorOutput) {
-        if (this.elevatorOutput != elevatorOutput) {
-            this.elevatorOutput = elevatorOutput;
+        if (desiredOutput != elevatorOutput) {
+            desiredOutput = elevatorOutput;
 
             if (isVelocity) {
-                //                System.out.println("elevator velocity: " + elevatorOutput);
-                this.elevator.set(ControlMode.Velocity, elevatorOutput);
+                elevatorMotor.set(ControlMode.Velocity, elevatorOutput);
             } else {
-                //                System.out.println("elevator power: " + elevatorOutput);
-                this.elevator.set(ControlMode.PercentOutput, elevatorOutput);
+                elevatorMotor.set(ControlMode.PercentOutput, elevatorOutput);
             }
         }
     }
@@ -103,32 +96,29 @@ public class Elevator extends Subsystem {
     }
 
     public double getDesiredOutput() {
-        return elevatorOutput;
+        return desiredOutput;
     }
 
     public boolean hasBallInElevator() {
         return !ballSensor.get();
     }
 
-    // TODO: implement when we have color sensors
-    public boolean colorOfBall() {
-        return true;
-    }
-
     @Override
     public void readFromHardware() {
         if (state != robotState.elevatorState) {
             if (isVelocity) {
-                actualOutput = elevator.getSelectedSensorVelocity(0);
+                actualOutput = elevatorMotor.getSelectedSensorVelocity(0);
+
                 if (Math.abs(actualOutput) < 100) { // TODO make this not a raw number
                     robotState.elevatorState = STATE.STOP;
                 } else if (state == STATE.INTAKE) {
                     robotState.elevatorState = state;
                 } else if (Math.abs(FIRE - actualOutput) < ALLOWABLE_ERROR) {
                     robotState.elevatorState = STATE.FIRE;
-                } else if (elevatorOutput < -1000) {
+                } else if (desiredOutput < -1000) {
                     robotState.elevatorState = STATE.FLUSH;
                 }
+
             } else {
                 robotState.elevatorState = state;
             }
@@ -153,13 +143,7 @@ public class Elevator extends Subsystem {
                     setElevator(FLUSH);
                     break;
             }
-            // create ball color updating here once sensor created
         }
-    }
-
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        //        builder.addBooleanProperty("Hopper/HasBall", this::hasBallInElevator, null);
     }
 
     @Override
@@ -168,11 +152,11 @@ public class Elevator extends Subsystem {
     @Override
     public boolean checkSystem() {
         boolean passed = true;
-        elevator.set(ControlMode.PercentOutput, 0.2);
+        elevatorMotor.set(ControlMode.PercentOutput, 0.2);
         Timer.delay(1);
-        elevator.set(ControlMode.PercentOutput, -0.2);
+        elevatorMotor.set(ControlMode.PercentOutput, -0.2);
         Timer.delay(1);
-        elevator.set(ControlMode.PercentOutput, 0);
+        elevatorMotor.set(ControlMode.PercentOutput, 0);
         return true;
     }
 
