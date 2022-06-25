1c9fb9359ddb58a78f18ab45694da597fbd5870f
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-06-25 12:24:33 -0500

    trapezoidal motion profile

---
+++ src/main/java/com/team1816/season/motion/profiles/TrapezoidalMotionProfile.java
@@ -0,0 +1,158 @@
+package com.team1816.season.motion.profiles;
+
+/**
+ * This class will construct a standard trapezoidal motion profile.
+ */
+public class TrapezoidalMotionProfile {
+
+    /**
+     * Internal Definitions
+     */
+
+    public static class Constraints {
+
+        private double maxVel, maxAccel, maxJerk;
+
+        public Constraints() {
+            maxVel = 0;
+            maxAccel = 0;
+            maxJerk = 0;
+        }
+
+        public Constraints(double mv, double ma) {
+            maxVel = mv;
+            maxAccel = ma;
+            maxJerk = 0;
+        }
+    }
+
+    public static class State {
+
+        public double position, velocity;
+
+        public State() {
+            position = 0;
+            velocity = 0;
+        }
+
+        public State(double p) {
+            position = p;
+            velocity = 0;
+        }
+
+        public State(double p, double v) {
+            position = p;
+            velocity = v;
+        }
+    }
+
+    public static class Phase {
+
+        public double duration;
+
+        public Phase() {
+            duration = 0;
+        }
+    }
+
+    private Phase[] p = new Phase[3];
+    private Constraints constraints;
+    private State initial;
+    private State target;
+
+    public TrapezoidalMotionProfile() {
+        p[0].duration = 0;
+        p[1].duration = 0;
+        p[2].duration = 0;
+        new TrapezoidalMotionProfile(new Constraints(), new State(), new State());
+    }
+
+    public TrapezoidalMotionProfile(Constraints c, State i, State t) {
+        constraints = c;
+        initial = i;
+        target = t;
+        double dX = t.position - i.position;
+        double dV = t.velocity - i.velocity;
+        double cv = c.maxVel;
+        if (dX < 0) {
+            c.maxAccel *= -1;
+        }
+        double t1 = 0;
+        double t2 = 0;
+        double t3 = 0;
+        do {
+            t1 = (cv - i.velocity) / c.maxAccel;
+            t3 = (cv - t.velocity) / c.maxAccel;
+
+            double edX = c.maxAccel * (t1 * t1 - t3 * t3) / 2 + (c.maxAccel * t1 + i.velocity) * t3;
+            t2 = (dX - edX) / (c.maxAccel * t1 + i.velocity);
+            cv /= 2.0;
+
+        } while (t2 >= 0);
+    }
+
+    public double getPosition(double t) {
+        double cx = initial.position;
+        double tmp = p[1].duration;
+        if (t <= tmp) {
+            cx += getAcceleration(t) / 2 * Math.pow((tmp - t), 2);
+            return cx;
+        }
+        cx += getAcceleration(tmp) / 2 * Math.pow(p[1].duration, 2);
+        tmp += p[2].duration;
+        if (t <= tmp) {
+            cx += getVelocity(t) * (tmp - t);
+            return cx;
+        }
+        cx += getVelocity(tmp) * (p[2].duration);
+        tmp += p[3].duration;
+        if (t <= tmp) {
+            cx += getVelocity(p[2].duration) * (tmp - t) + getAcceleration(t) / 2 * Math.pow((tmp - t), 2);
+            return cx;
+        } else {
+            return target.position;
+        }
+    }
+
+    public double getVelocity(double t) {
+        double cv = initial.velocity;
+        double tmp = p[1].duration;
+        if (t <= tmp) {
+            cv += getAcceleration(t) * (tmp - t);
+            return cv;
+        }
+        cv += getAcceleration(tmp) * p[1].duration;
+        tmp += p[2].duration;
+        if (t <= tmp) {
+            return cv;
+        }
+        tmp += p[3].duration;
+        if (t <= tmp) {
+            cv += getAcceleration(t) * (tmp - t);
+            return cv;
+        } else {
+            return target.velocity;
+        }
+    }
+
+    public double getAcceleration(double t) {
+        double tmp = p[1].duration;
+        if (t <= tmp) {
+            return constraints.maxAccel;
+        }
+        tmp += p[2].duration;
+        if (t <= tmp) {
+            return 0;
+        }
+        tmp += p[3].duration;
+        if (t <= tmp) {
+            return (-1) * constraints.maxAccel;
+        } else {
+            return 0;
+        }
+    }
+
+    public double getJerk(double t) {
+        return 0;
+    }
+}
