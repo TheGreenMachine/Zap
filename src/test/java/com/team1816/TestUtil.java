1ec5641963b8c2edf4b50bb235f350fc872b0175
David Day <dcday@themilwaukeeroad.com>
2022-08-09 19:00:04 -0500

    update base mocks
    all tests now no longer have inject errors now just failed tests for wrong values

--- src/test/java/com/team1816/TestUtil.java
+++ src/test/java/com/team1816/TestUtil.java
@@ -5,11 +5,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.team1816.lib.Injector;
+import com.team1816.lib.hardware.PIDSlotConfiguration;
 import com.team1816.lib.hardware.SubsystemConfig;
 import com.team1816.lib.hardware.components.IPigeonIMU;
 import com.team1816.lib.hardware.components.motor.IGreenMotor;
 import com.team1816.lib.hardware.components.pcm.ICompressor;
 import com.team1816.lib.hardware.factory.RobotFactory;
+import com.team1816.lib.subsystems.SwerveModule;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class TestUtil {
 
@@ -30,6 +33,18 @@ public class TestUtil {
         var config = new SubsystemConfig();
         config.implemented = false;
         when(mockFactory.getSubsystem(anyString())).thenReturn(config);
+        var slotConfig = new PIDSlotConfiguration();
+        slotConfig.kD = 0d;
+        slotConfig.kP = 0d;
+        slotConfig.kI = 0d;
+        slotConfig.kF = 0d;
+        slotConfig.allowableError = 100d;
+        slotConfig.iZone = 1;
+        when(mockFactory.getPidSlotConfig(anyString())).thenReturn(slotConfig);
+        var sm = mock(SwerveModule.class);
+        when(sm.getActualState()).thenReturn(new SwerveModuleState());
+        when(mockFactory.getSwerveModule(anyString(), anyString()))
+            .thenAnswer(input -> sm);
         Injector.register(mockFactory);
     }
 }
