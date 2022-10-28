5a8aa2e6dfa7b719a28b2da26a58b9d9edc60ccb
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-10-27 20:05:23 -0500

    updated April-tag positions, added documentation, and re-organization

--- src/main/java/com/team1816/season/Constants.java
+++ src/main/java/com/team1816/season/Constants.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.util.Units;
+
 import java.util.HashMap;
 
 /**
@@ -15,18 +16,24 @@ import java.util.HashMap;
 @Singleton
 public class Constants {
 
-    /** Factory & Stem */
+    /**
+     * Factory & Stem
+     */
     private static final RobotFactory factory = Robot.getFactory();
 
     public static final Pose2d EmptyPose = new Pose2d();
     public static final Rotation2d EmptyRotation = new Rotation2d();
     public static final double kLooperDt = factory.getConstant("kLooperDt", .020);
 
-    /** CAN Timeouts */
+    /**
+     * CAN Timeouts
+     */
     public static final int kCANTimeoutMs = 10; // use for important on the fly updates
     public static final int kLongCANTimeoutMs = 100; // use for constructors
 
-    /** Field characterization */
+    /**
+     * Field characterization
+     */
     public static final double kTargetHeight = 104; // inches
     public static final double kTargetRadius = 26.56; // inches
     public static final double kCameraMountingHeight = 29.5; // inches
@@ -53,11 +60,11 @@ public class Constants {
     public static final HashMap<Integer, Double[]> fieldTargets = new HashMap<>() {
         {
             /**
-             * retro-reflective tape targets
+             * Retro-reflective tape targets
              * */
             put(
                 -1,
-                new Double[] {
+                new Double[]{
                     Constants.fieldCenterX,
                     Constants.fieldCenterY,
                     Units.inchesToMeters(Constants.kTargetHeight),
@@ -67,17 +74,63 @@ public class Constants {
             /**
              * April Tags
              */
-            put(03, new Double[] { 3.219, 5.493, 1.725, 0.0 });
+            put(01, new Double[]{3.219, 5.493, 1.725});
 
-            put(11, new Double[] { 13.240, 2.75, 1.725, 0.0 });
+            put(11, new Double[]{13.240, 2.75, 1.725});
 
-            put(50, new Double[] { 7.684, 4.330, 2.408, 1.0 });
+            // blue alliance side
+            put(00, new Double[] {0.000, 7.510, 0.886});
 
-            put(51, new Double[] { 8.02, 3.576, 2.408, 1.0 });
+            put(01, new Double[] {3.270, 5.588, 1.725}); // upper hangar target
 
-            put(52, new Double[] { 8.775, 3.912, 2.408, 1.0 });
+            put(02, new Double[] {3.072, 5.249, 1.376}); // lower hangar target
 
-            put(53, new Double[] { 8.439, 4.667, 2.408, 1.0 });
+            put(03, new Double[] {0.008, 5.116, 1.376});
+
+            put(04, new Double[] {0.008, 3.575, 1.376});
+
+            // blue human player station
+            put(05, new Double[] {0.125, 0.829, 0.891});
+
+            put(06, new Double[] {0.877, 0.879, 0.891});
+
+            put(07, new Double[] {1.619, 0.095, 0.891});
+
+            // red alliance side
+            put(10, new Double[]{16.467, 0.589, 0.886});
+
+            put(11, new Double[]{13.240, 2.750, 1.725}); // upper hangar target
+
+            put(12, new Double[]{13.395, 2.838, 1.376}); // lower hangar target
+
+            put(13, new Double[]{16.459, 3.114, 0.806});
+
+            put(14, new Double[]{16.459, 4.655, 0.806});
+
+            // red human player station
+            put(15, new Double[]{16.339, 6.453, 0.894});
+
+            put(16, new Double[]{15.594, 7.231, 0.891});
+
+            put(17, new Double[]{14.851, 8.007, 0.891});
+
+            // lower hub targets
+            put(40, new Double[]{7.878, 4.851, 0.703});
+
+            put(41, new Double[]{7.435, 3.697, 0.703});
+
+            put(42, new Double[]{8.589, 3.254, 0.703});
+
+            put(43, new Double[]{9.032, 4.408, 0.703});
+
+            // upper hub targets
+            put(50, new Double[]{7.684, 4.330, 2.408});
+
+            put(51, new Double[]{8.020, 3.576, 2.408});
+
+            put(52, new Double[]{8.775, 3.912, 2.408});
+
+            put(53, new Double[]{8.439, 4.667, 2.408});
         }
     };
     public static final Pose2d kDefaultZeroingPose = new Pose2d(
@@ -86,11 +139,15 @@ public class Constants {
         EmptyRotation
     );
 
-    /** Drivetrain characterization */
+    /**
+     * Drivetrain characterization
+     */
     public static final double gravitationalAccelerationConstant = 9.8d;
     public static double kMaxAccelDiffThreshold = 2d; // m/s^2
 
-    /** Badlog */
+    /**
+     * Badlog
+     */
     public static boolean kIsBadlogEnabled = factory.getConstant("badLogEnabled") > 0;
     public static boolean kIsLoggingTeleOp = factory.getConstant("logTeleOp") > 0;
     public static boolean kIsLoggingAutonomous = factory.getConstant("logAuto") > 0;
