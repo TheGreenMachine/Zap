949659ab4ddaee373e1828d9e615711d6dcc722a
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-11-12 14:55:37 -0600

    temp

--- src/main/java/com/team1816/season/subsystems/Camera.java
+++ src/main/java/com/team1816/season/subsystems/Camera.java
@@ -95,7 +95,7 @@ public class Camera extends Subsystem {
             }
         }
         GreenPhotonCamera.setVersionCheckEnabled(false);
-        cam = new PhotonCamera("Microsoft_LifeCam_HD-3000");
+        cam = new PhotonCamera("microsoft");
     }
 
     public void setCameraEnabled(boolean cameraEnabled) {
@@ -144,43 +144,43 @@ public class Camera extends Subsystem {
 
     public ArrayList<Point> getPoints() {
         ArrayList<Point> targets = new ArrayList<>();
-        var result = cam.getLatestResult().getBestTarget();
-        Point p = new Point();
+        var result = cam.getLatestResult();
+
         if (result == null) {
-            System.out.println("I'm blind");
+            System.out.println();
+            if (result.targets == null) {
+                System.out.println("Pipeline targets null");
+                if (result.targets.size() == 0) {
+                    System.out.println("Pipeline Empty");
+                }
+            }
+        }
+
+        if (!result.hasTargets()) {
             return targets;
         }
-        p.id = result.getFiducialId();
-        p.cameraToTarget = result.getCameraToTarget();
-        targets.add(p);
-        System.out.println(targets);
+
+        double m = 0xFFFFFF; // big number
+        var principal_RANSAC = new PhotonTrackedTarget();
+
+        for (PhotonTrackedTarget target : result.targets) {
+            var p = new Point();
+            if (target.getCameraToTarget() != null) {
+                p.cameraToTarget = target.getCameraToTarget();
+                p.id = target.getFiducialId();
+                targets.add(p);
+
+                if (m > p.cameraToTarget.getTranslation().getNorm()) {
+                    m = p.cameraToTarget.getTranslation().getNorm();
+                    principal_RANSAC = target;
+                }
+            }
+        }
+        System.out.println(targets.get(0).cameraToTarget.toString()); //TODO Remove
+
+        bestTrackedTarget = principal_RANSAC;
+
         return targets;
-        //        var result = cam.getLatestResult();
-        //        if (!result.hasTargets()) {
-        //            return targets;
-        //        }
-        //
-        //        double m = 0xFFFFFF; // big number
-        //        var principal_RANSAC = new PhotonTrackedTarget();
-        //
-        //        for (PhotonTrackedTarget target : result.targets) {
-        //            var p = new Point();
-        //            if (target.getCameraToTarget() != null) {
-        //                p.cameraToTarget = target.getCameraToTarget();
-        //                p.id = target.getFiducialId();
-        //                targets.add(p);
-        //
-        //                if (m > p.cameraToTarget.getTranslation().getNorm()) {
-        //                    m = p.cameraToTarget.getTranslation().getNorm();
-        //                    principal_RANSAC = target;
-        //                }
-        //            }
-        //        }
-        //        System.out.println(targets.get(0).cameraToTarget.toString()); //TODO Remove
-        //
-        //        bestTrackedTarget = principal_RANSAC;
-        //
-        //        return targets;
     }
 
     public double getDistance() {
