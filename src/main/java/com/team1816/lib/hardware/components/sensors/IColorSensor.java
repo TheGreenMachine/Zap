7074918cdd3bfbacf43514ca7aa896e2d2e00fbd
georgealanzo <georerat@gmail.com>
2022-12-07 12:19:25 -0600

    IColorSensor class created and updated

---
+++ src/main/java/com/team1816/lib/hardware/components/sensors/IColorSensor.java
@@ -0,0 +1,15 @@
+package com.team1816.lib.hardware.components.sensors;
+import edu.wpi.first.wpilibj.util.Color;
+
+public interface IColorSensor {
+
+    int getId();
+
+    Color getColor();
+
+    int getRed();
+
+    int getGreen();
+
+    int getBlue();
+}
