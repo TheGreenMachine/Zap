e21d2d8b3e4cdaae6be69b35af842fead2ba0aeb
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-12-07 13:29:25 -0600

    ghost handling

--- src/main/java/com/team1816/lib/hardware/factory/GreenSensorFactory.java
+++ src/main/java/com/team1816/lib/hardware/factory/GreenSensorFactory.java
@@ -1,15 +1,21 @@
 package com.team1816.lib.hardware.factory;
 
 import com.revrobotics.ColorSensorV3;
+import com.team1816.lib.hardware.components.sensors.GhostColorSensor;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.I2C;
 
 public class GreenSensorFactory {
 
-    public ColorSensorV3 createColorSensor(I2C.Port port) {
-        var sensor = new ColorSensorV3(port);
-        configureColorSensorV3(sensor);
-        return sensor;
+    public ColorSensorV3 createColorSensor(I2C.Port port, boolean ghost) { // Robot factory will tell it to ghost so if port = -1, make it 0
+        if (ghost) {
+            var sensor = new GhostColorSensor(port);
+            return sensor;
+        } else {
+            var sensor = new ColorSensorV3(port);
+            configureColorSensorV3(sensor);
+            return sensor;
+        }
     }
 
     private void configureColorSensorV3(ColorSensorV3 sensor) {
