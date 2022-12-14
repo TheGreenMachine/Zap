f92bc8d8c2552142ac52068cac33114db3a52a78
Keerthi Kaashyap <keerthik23939@isd273.org>
2022-12-14 16:44:51 -0600

    completed documentation of lib.auto

--- src/main/java/com/team1816/lib/auto/paths/AutoPath.java
+++ src/main/java/com/team1816/lib/auto/paths/AutoPath.java
@@ -6,20 +6,45 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import java.util.List;
 
 /**
- * Interface containing all information necessary for a path including the Path itself, the Path's starting pose, and
- * whether or not the robot should drive in reverse along the path.
+ * Abstract class containing all information necessary for running a trajectory as a TrajectoryAction
+ * @see com.team1816.lib.auto.actions.TrajectoryAction
+ * @see Trajectory
  */
 public abstract class AutoPath {
 
+    /**
+     * State: contains information about the trajectory that defines the path
+     */
     Trajectory trajectory;
+    /**
+     * State: headings corresponding to the path that can be transposed onto the trajectory for omni-directional drivetrains like swerve
+     */
     List<Rotation2d> headings;
 
+    /**
+     * Returns a list of Pose2d's that define the trajectory /path to be followed
+     * @return waypoints
+     */
     protected abstract List<Pose2d> getWaypoints();
 
+    /**
+     * Returns a list of Rotation2d's corresponding to the rotation respect to the trajectory
+     * @return waypointHeadings
+     */
     protected abstract List<Rotation2d> getWaypointHeadings();
 
-    protected abstract boolean usingApp(); // just to check whether to add the starting pose of .5 X and 3.5 Y
+    /**
+     * Checks if the path was made using the CheesyPath web application.
+     * If false, the starting pose of the trajectory will be set to the default starting pose.
+     * @return boolean usingApp
+     */
+    protected abstract boolean usingApp();
 
+    /**
+     * Returns the generated trajectory associated with the AutoPath
+     * @return trajectory
+     * @see Trajectory
+     */
     public Trajectory getAsTrajectory() {
         if (trajectory == null) {
             trajectory = PathUtil.generateTrajectory(usingApp(), getWaypoints());
@@ -27,6 +52,11 @@ public abstract class AutoPath {
         return trajectory;
     }
 
+    /**
+     * Returns the list of trajectory headings that are transposed onto the path, ignored for tank based drivetrains
+     * @return trajectoryHeadings
+     * @see com.team1816.lib.auto.actions.TrajectoryAction
+     */
     public List<Rotation2d> getAsTrajectoryHeadings() {
         if (headings == null) {
             headings =
--- src/main/java/com/team1816/lib/auto/paths/PathUtil.java
+++ src/main/java/com/team1816/lib/auto/paths/PathUtil.java
@@ -15,29 +15,41 @@ import java.util.List;
 
 public class PathUtil {
 
+    /** Constraints */
     private static final double kMaxVelocity = kPathFollowingMaxVelMeters;
     private static final double kMaxAccel = kPathFollowingMaxAccelMeters;
 
+    /**
+     * Generates a trajectory based on a list of waypoints based on WPIlib's TrajectoryGenerator
+     * @param usingApp
+     * @param waypoints
+     * @return trajectory
+     * @see com.team1816.lib.auto.modes.AutoMode
+     * @see AutoPath
+     * @see edu.wpi.first.math.trajectory.TrajectoryGenerator
+     */
     public static Trajectory generateTrajectory(
         boolean usingApp,
         List<Pose2d> waypoints
     ) {
+        /* Inch to meter conversions for waypoints for trajectory calculations */
         List<Pose2d> waypointsMeters = new ArrayList<>();
         for (Pose2d pose2d : waypoints) {
             waypointsMeters.add(
                 new Pose2d(
-                    // manually adding the starting pose to each waypoint - wpilib transformBy acts up when transforming FiveBall - bug?
-                    Units.inchesToMeters(pose2d.getX()), // + Constants.StartingPose.getTranslation().getX(),
-                    Units.inchesToMeters(pose2d.getY()), // + Constants.StartingPose.getTranslation().getY(),
-                    pose2d.getRotation() // .plus(Constants.StartingPose.getRotation())
+                    Units.inchesToMeters(pose2d.getX()),
+                    Units.inchesToMeters(pose2d.getY()),
+                    pose2d.getRotation()
                 )
             );
         }
+        /* Configures trajectory constraints */
         TrajectoryConfig config = new TrajectoryConfig(kMaxVelocity, kMaxAccel);
         var baseTrajectory = edu.wpi.first.math.trajectory.TrajectoryGenerator.generateTrajectory(
             waypointsMeters,
             config
         );
+        /* If web application is not used, then the starting pose is transformed to the default starting pose, this has no impact on how the trajectory is run */
         if (!usingApp) {
             baseTrajectory =
                 baseTrajectory.transformBy(
@@ -50,6 +62,13 @@ public class PathUtil {
         return baseTrajectory;
     }
 
+    /**
+     * Generates headings that can be transposed onto a trajectory with time calibration via a differential model
+     * @param usingApp
+     * @param waypoints
+     * @param swerveHeadings
+     * @return headings
+     */
     public static List<Rotation2d> generateHeadings(
         boolean usingApp,
         List<Pose2d> waypoints,
@@ -58,15 +77,18 @@ public class PathUtil {
         if (waypoints == null || swerveHeadings == null) {
             return null;
         }
+
         double startX = .5;
         double startY = Constants.fieldCenterY;
 
+        /* Trajectory is generated */
         Trajectory trajectory = generateTrajectory(usingApp, waypoints);
         List<Pose2d> waypointsMeters = new ArrayList<>();
         if (usingApp) {
             startX = 0;
             startY = 0;
         }
+        /* Inch to meter conversions */
         for (Pose2d pose2d : waypoints) {
             waypointsMeters.add(
                 new Pose2d(
@@ -77,14 +99,14 @@ public class PathUtil {
             );
         }
 
-        //get times and indices
+        /* Time constraint calibration */
         List<Double> waypointTimes = new ArrayList<>();
         List<Integer> waypointIndexes = new ArrayList<>();
         int iWaypointCheckpoint = 0;
         for (Pose2d waypointPose2d : waypointsMeters) {
             for (int i = iWaypointCheckpoint; i < trajectory.getStates().size(); i++) {
                 var trajectoryPose2d = trajectory.getStates().get(i).poseMeters;
-                if (trajectoryPose2d.equals(waypointPose2d)) { // Conversions.epsilonEquals(point, pose2d, .0001
+                if (trajectoryPose2d.equals(waypointPose2d)) {
                     waypointTimes.add(trajectory.getStates().get(i).timeSeconds);
                     waypointIndexes.add(i);
                     break;
@@ -93,7 +115,7 @@ public class PathUtil {
             }
         }
 
-        // generate list of rotation2Ds equivalent to trajectory length - for each state a heading
+        /* list of headings generated with equivalent length to the trajectory poses */
         List<Rotation2d> generatedHeadings = new ArrayList<>();
         for (
             int nextCheckpoint = 1;
@@ -102,29 +124,29 @@ public class PathUtil {
         ) {
             int iStart = waypointIndexes.get(nextCheckpoint - 1);
             int iEnd = waypointIndexes.get(nextCheckpoint);
-            double totalDHeading =
-                ( // total change in heading between two points in degrees
+            double totalDHeading = (
                     swerveHeadings.get(nextCheckpoint).getDegrees() -
                     swerveHeadings.get(nextCheckpoint - 1).getDegrees()
                 );
             double timeBetweenWaypoints =
                 waypointTimes.get(nextCheckpoint) - waypointTimes.get(nextCheckpoint - 1);
-            double dHeading = totalDHeading / timeBetweenWaypoints; // change in heading per state
+            double dHeading = totalDHeading / timeBetweenWaypoints;
 
             for (int i = iStart; i < iEnd; i++) {
                 generatedHeadings.add(
                     Rotation2d.fromDegrees(
-                        swerveHeadings.get(nextCheckpoint - 1).getDegrees() + // last waypoint's heading in degrees
+                        swerveHeadings.get(nextCheckpoint - 1).getDegrees() +
                         dHeading *
                         (
                             trajectory.getStates().get(i).timeSeconds -
                             waypointTimes.get(nextCheckpoint - 1)
-                        ) // change in heading * current time between waypoints
+                        )
                     )
                 );
             }
         }
-        // add the end rotation to make sure that last rotation imput is the last heading - band-aid fix.
+
+        /* Adds final heading to be the same as the previous so the path is aligned with the heading */
         generatedHeadings.add(swerveHeadings.get(swerveHeadings.size() - 1));
 
         return generatedHeadings;
